0cb8b557b464694afc681b8f44bc395f72df8f20
Gabriel Garrido <gabrielgarridoag@gmail.com>
2021-02-20 02:26:19 -0300

    Obtendo usuário por id.

--- src/main/java/com/garrido/api/services/UserService.java
+++ src/main/java/com/garrido/api/services/UserService.java
@@ -1,12 +1,14 @@
 package com.garrido.api.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.garrido.api.domain.User;
 import com.garrido.api.repository.UserRepository;
+import com.garrido.api.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,9 @@ public class UserService {
 		return repository.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Error! Object not found!"));
+	}
 }
